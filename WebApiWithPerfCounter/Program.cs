6fcebb75a042a6b6ad64a92b232b002471c65415
unknown <alexb@kiepc4298.INTERN.CRYTEK.DE>
2016-06-07 18:41:36 +0300

    init repo

---
+++ WebApiWithPerfCounter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Threading;
+
+using Microsoft.Owin.Hosting;
+
+namespace WebApiWithPerfCounter
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+			using (var webHost = WebApp.Start<Startup>("http://127.0.0.1:5000/"))
+			{
+				var process = Process.GetCurrentProcess();
+				using (var counter = new PerformanceCounter("Process", "Thread Count", process.ProcessName, true))
+				{
+					Console.WriteLine(counter.NextValue());
+				}
+
+				PerformanceCounter.CloseSharedResources();
+
+				Console.ReadLine();
+
+				Console.WriteLine("Exit1");
+			}
+
+			PerformanceCounter.CloseSharedResources();
+
+			Console.WriteLine("Exit2");
+		}
+	}
+}
---
+++ WebApiWithPerfCounter/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+
+using Owin;
+
+namespace WebApiWithPerfCounter
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder appBuilder)
+		{
+			var config = new HttpConfiguration();
+
+			// Web API routes
+			config.MapHttpAttributeRoutes();
+			config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.LocalOnly;
+		}
+	}
+}
